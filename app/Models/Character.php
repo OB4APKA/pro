22c4fc75b753c80c3d595f68bf1095879fa92933
OB4APKA <pila8922@gmail.com>
2025-12-10 14:17:25 +0500

    soft delete and mutatuions update

--- app/Models/Character.php
+++ app/Models/Character.php
@@ -3,9 +3,12 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Character extends Model
 {
+    use SoftDeletes;
+
     protected $fillable = [
         'name',
         'type',
@@ -13,4 +16,21 @@ class Character extends Model
         'description',
         'detailed_description'
     ];
+
+    public function setNameAttribute($value)
+    {
+        $this->attributes['name'] = ucfirst(trim($value));
+    }
+
+
+    public function setTypeAttribute($value)
+    {
+        $this->attributes['type'] = strtolower(trim($value));
+    }
+
+
+    public function getTypeAttribute($value)
+    {
+        return ucfirst($value);
+    }
 }
